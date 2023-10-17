6cd3fdc2419732f0cbb755310dfcbb14f0cf162f
AlexSkilled <olegthander@yandex.ru>
2023-10-17 14:27:48 +0300

    [init] чистки

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -18,15 +18,17 @@ export function Home() {
     return (
         <div className={cls.Home}>
             <div className={cls.sidebarWrap}>
-
+                Боковое меню-дерево
             </div>
             <div className={cls.contentWrap}>
+                Контент
                 {/*<Routes>*/}
                 {/*    <Route path={"/*"} element={<ContentWrapper content={pageContent}/>}/>*/}
                 {/*</Routes>*/}
             </div>
 
             <div className={cls.sideMenuWrap}>
+                Боковое меню Поп-ап
                 {/*<Sidebar setContentViaLink={getContentViaLink}/>*/}
             </div>
 

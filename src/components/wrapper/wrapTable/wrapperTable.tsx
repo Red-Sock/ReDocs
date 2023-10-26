ecd24ad6237fcfd2f9b49024d9176082e45e5ac2
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-10-26 18:33:56 +0400

    [RD-7]переместил в компоненты, доделал обертку для таблицы

---
+++ src/components/wrapper/wrapTable/wrapperTable.tsx
@@ -0,0 +1,46 @@
+import "./wrapperTable.css";
+
+function WrapperTable(props: { name: string[]; value: string[][] }) {
+  // function deleteHTML(e: any) {
+  //   const prop: any = e;
+  //   const matches: string[] = [];
+  //   let match: any;
+  //   const pattern = /<[^>]+>([^<]+)/g;
+  //   while ((match = pattern.exec(prop)) !== null) {
+  //     matches.push(match[1]);
+  //     return matches;
+  //   }
+  //   return prop;
+  // } удаляет теги из текста
+
+  return (
+    <div>
+      <table className="tableWrap">
+        <thead>
+          <tr className="tableLine">
+            {props.name.map((e) => {
+              return <th className="tableTitleText">{e}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {props.value.map((e) => {
+            return (
+              <tr className="tableLine">
+                {e.map((e) => {
+                  return (
+                    <td
+                      className="tableText"
+                      dangerouslySetInnerHTML={{ __html: e }}
+                    />
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default WrapperTable;

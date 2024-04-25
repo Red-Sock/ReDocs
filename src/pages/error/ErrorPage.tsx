31cc514b454cb8bb2b39c94561d0ae22324a9c70
AlexSkilled <olegthander@yandex.ru>
2024-04-25 20:33:35 +0300

    [main] tests n staff

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,9 +1,7 @@
 import cls from './ErrorPage.module.css'
 
 import {isRouteErrorResponse, useRouteError} from "react-router-dom";
-import {Copy} from "../../assets/img/copy";
 import {CopyButton} from '../../components/copyButton/copyButton';
-import {configState} from "../../state/config";
 
 export function ErrorPage() {
     const err = useRouteError();
